59fe821636b14168681b87b09043b61079aa8995
Sánchez Boto <msboto@indra.es>
2024-03-07 18:52:44 +0100

    added files mjs

---
+++ cjs/sum.js
@@ -0,0 +1,6 @@
+function sum(a, b){
+    return a + b
+}
+
+//CommonJS export module
+module.exports = { sum }
---
+++ index.mjs
@@ -0,0 +1,6 @@
+// .js --> utiliza por defecto Common JS
+// .mjs --> para utilizar ES Modules
+// .cjs --> para utilizar Common JS
+
+import { sum } from './sum.mjs'
+console.log(sum(1,2))
